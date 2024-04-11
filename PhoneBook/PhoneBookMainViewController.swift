2828ad12f372fb06a97eed1a0171ba6b47c7b208
ibrahimm96 <ibrahimm96252@gmail.com>
2024-04-11 15:45:39 -0700

    Added TextField with Numberpad for desired phone number

--- PhoneBook/PhoneBookMainViewController.swift
+++ PhoneBook/PhoneBookMainViewController.swift
@@ -7,16 +7,35 @@
 
 import UIKit
 
-class PhoneBookMainViewController: UIViewController {
+class PhoneBookMainViewController: UIViewController{
+
     @IBOutlet var openTextMessageButton: UIButton!
+    @IBOutlet var phoneNumberInput: UITextField!
     
     var recipient = "sms:19172883884"
+    
     @IBAction func textButtonTapped(_ sender: Any) {
-        UIApplication.shared.open(URL(string: recipient)!, options: [:], completionHandler: nil)
+        if let phoneNumber = phoneNumberInput.text, !phoneNumber.isEmpty {
+            let recipient = "sms:" + phoneNumber
+            if let url = URL(string: recipient) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+            endEditing()
+        } else {
+            print("Invalid Number")
+        }
     }
+    
+    func endEditing() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        phoneNumberInput.keyboardType = .numberPad
     }
 
 
