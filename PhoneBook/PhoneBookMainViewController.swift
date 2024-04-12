ace04e1f1f7df179c8ee67da13adbad61ecd56d3
ibrahimm96 <ibrahimm96252@gmail.com>
2024-04-11 23:04:59 -0700

    Added two methods for opening messages and whatsApp. Also added back showAlert()

--- PhoneBook/PhoneBookMainViewController.swift
+++ PhoneBook/PhoneBookMainViewController.swift
@@ -10,26 +10,47 @@ import CoreLocation
 
 class PhoneBookMainViewController: UIViewController, CLLocationManagerDelegate {
     
+    
     private let locationManager = CLLocationManager()
     @IBOutlet var openTextMessageButton: UIButton!
     var defaultPhoneNumber = "15103301270"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
     @IBAction func textButtonTapped(_ sender: Any) {
         
     }
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
         let geocoderLocation = CLGeocoder()
+    }
+    
+    func openMessagesApp() {
+        guard let url = URL(string: "sms::15103301270") else {
+            showAlert(message: "Failed to open Messages app")
+            return
         }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    
+    func openWhatsApp(phoneNumber: String) {
+        guard let url = URL(string: "whatsapp://send?phone=15103301270") else {
+            showAlert(message: "Failed to open WhatsApp")
+            return
+        }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    
 }
     
 
