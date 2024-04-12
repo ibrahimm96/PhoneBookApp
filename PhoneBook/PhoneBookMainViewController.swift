858d74acb04273a471ac23535a934e8426f1cb71
ibrahimm96 <ibrahimm96252@gmail.com>
2024-04-12 01:23:06 -0700

    Refactored code, added UI for QRCode image button

--- PhoneBook/PhoneBookMainViewController.swift
+++ PhoneBook/PhoneBookMainViewController.swift
@@ -10,30 +10,37 @@ import CoreLocation
 
 class PhoneBookMainViewController: UIViewController, CLLocationManagerDelegate {
     
-    var locationManager: CLLocationManager?
     @IBOutlet var openTextMessageButton: UIButton!
-    var defaultPhoneNumber = "15103301270"
+    
+    var locationManager: CLLocationManager?
+    let desiredCounty = "United States"
+    let smsURL = "sms::15103301270"
+    let whatsAppURL = "whatsapp://send?phone=15103301270"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         locationManager = CLLocationManager()
         locationManager?.requestWhenInUseAuthorization()
-        
+    }
+    
+
+
+    @IBAction func textButtonTapped(_ sender: Any) {
         findCountry { country in
             if let country = country {
                 print("User is in \(country)")
                 // Perform additional actions based on the user's country
             } else {
                 print("Unable to determine user's country")
-                // Handle the case where the country couldn't be determined
+            }
+            
+            if country != self.desiredCounty {
+                self.openMessagesApp()
+            } else {
+                self.openWhatsApp()
             }
         }
-    }
-    
-
-
-    @IBAction func textButtonTapped(_ sender: Any) {
-        
     }
     
     func showAlert(message: String) {
@@ -41,6 +48,41 @@ class PhoneBookMainViewController: UIViewController, CLLocationManagerDelegate {
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(okAction)
         present(alert, animated: true, completion: nil)
+    }
+    
+    func openMessagesApp() {
+        guard let url = URL(string: smsURL) else {
+            showAlert(message: "Failed to open Messages app")
+            return
+        }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    
+    func openWhatsApp() {
+        guard let url = URL(string: whatsAppURL) else {
+            showAlert(message: "Failed to open WhatsApp")
+            return
+        }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    
+}
+
+extension PhoneBookMainViewController{
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .notDetermined:
+            print("When user did not yet determined")
+        case .restricted:
+            print("Restricted by parental control")
+        case .denied:
+            print("When user select option Dont't Allow")
+        case .authorizedWhenInUse:
+            print("When user select option Allow While Using App or Allow Once")
+        default:
+            print("default")
+        }
     }
     
     func findCountry(completion: @escaping (String?) -> Void) {
@@ -76,40 +118,6 @@ class PhoneBookMainViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
 
-    
-    func openMessagesApp() {
-        guard let url = URL(string: "sms::15103301270") else {
-            showAlert(message: "Failed to open Messages app")
-            return
-        }
-        UIApplication.shared.open(url, options: [:], completionHandler: nil)
-    }
-    
-    func openWhatsApp() {
-        guard let url = URL(string: "whatsapp://send?phone=15103301270") else {
-            showAlert(message: "Failed to open WhatsApp")
-            return
-        }
-        UIApplication.shared.open(url, options: [:], completionHandler: nil)
-    }
-    
-}
-
-extension PhoneBookMainViewController {
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        switch manager.authorizationStatus {
-        case .notDetermined:
-            print("When user did not yet determined")
-        case .restricted:
-            print("Restricted by parental control")
-        case .denied:
-            print("When user select option Dont't Allow")
-        case .authorizedWhenInUse:
-            print("When user select option Allow While Using App or Allow Once")
-        default:
-            print("default")
-        }
-    }
 }
 
     
