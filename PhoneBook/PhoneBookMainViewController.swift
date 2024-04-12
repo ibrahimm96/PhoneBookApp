35e203403923094e34bfb8b3f976bb5a211ab029
ibrahimm96 <ibrahimm96252@gmail.com>
2024-04-11 18:54:09 -0700

    Revised conditional statements for invalid phone number cases. Used "guard" instead of "if/else" and added additional error handling scenarios for phone number input.

--- PhoneBook/PhoneBookMainViewController.swift
+++ PhoneBook/PhoneBookMainViewController.swift
@@ -8,36 +8,56 @@
 import UIKit
 
 class PhoneBookMainViewController: UIViewController{
-
+    
     @IBOutlet var openTextMessageButton: UIButton!
     @IBOutlet var phoneNumberInput: UITextField!
     
     var recipient = "sms:19172883884"
     
     @IBAction func textButtonTapped(_ sender: Any) {
-        if let phoneNumber = phoneNumberInput.text, !phoneNumber.isEmpty {
-            let recipient = "sms:" + phoneNumber
-            if let url = URL(string: recipient) {
-                UIApplication.shared.open(url, options: [:], completionHandler: nil)
-            }
-            endEditing()
-        } else {
-            print("Invalid Number")
+        guard let phoneNumber = phoneNumberInput.text?.trimmingCharacters(in: .whitespacesAndNewlines), !phoneNumber.isEmpty else {
+            showAlert(message: "Phone number cannot be empty")
+            return
         }
+        
+        let numericPhoneNumber = phoneNumber.replacingOccurrences(of: "[^0-9]", with: "", options: .regularExpression)
+        
+        guard numericPhoneNumber.count == 10 else {
+            showAlert(message: "Invalid Number")
+            return
+        }
+        
+        let recipient = "sms:" + numericPhoneNumber
+        
+        guard let url = URL(string: recipient) else {
+            showAlert(message: "Invalid URL")
+            return
+        }
+        
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        view.endEditing(true) 
     }
     
-    func endEditing() {
-        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "Invalid Number!", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
     }
-
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         phoneNumberInput.keyboardType = .numberPad
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tapGesture)
     }
-
-
+    
 }
 
